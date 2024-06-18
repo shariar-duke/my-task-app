3587c6505c9da497dc7fdb2791a6f7e5a7e539da
shariar-duke <shariarmahmud225@gmail.com>
2024-06-18 10:41:41 +0600

    Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+export default function Header() {
+    return (
+      <nav className="py-2 px-12 md:py-4 w-full z-50 fixed top-0 bg-[#191D26]">
+        <div className="container mx-auto flex items-center justify-between   ">
+         <a href="/">
+            <img src="/public/lws-logo-en.svg" alt="logo" />
+         </a>
+        </div>
+      </nav>
+    )
+  }
+  
