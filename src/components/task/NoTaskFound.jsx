d367db995a8ca08bba51dff7624afd05507f31ed
shariar-duke <shariarmahmud225@gmail.com>
2024-06-18 14:52:29 +0600

    No Task Found component

---
+++ src/components/task/NoTaskFound.jsx
@@ -0,0 +1,9 @@
+
+
+export default function NoTaskFound() {
+  return (
+    <div className="mt-[25px] mb-[50px]">
+      <p className="text-white text-center">No Task Found, Please add one</p>
+    </div>
+  );
+}
