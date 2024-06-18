5ab8668212a6004af0132e5e0bca649bc83f7989
shariar-duke <shariarmahmud225@gmail.com>
2024-06-18 19:28:54 +0600

    task board with one data explicity

--- src/components/task/TaskBorad.jsx
+++ src/components/task/TaskBorad.jsx
@@ -1,15 +1,30 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
 
 
 export default function TaskBorad() {
+ const [show, setShow] = useState(false)
+ const defaultTask = {
+    'id':crypto.randomUUID(), // eta amk random Id dey 
+    'ititle': "Learn Reacat",
+    "description" : "I want to learn react as much as I can. Then  I will try to learn the other things ",
+    "tags":["web", "react","js"],
+    "priority":"High",
+    "isFavourite":true
+ }
+ const [tasks,setTasks] = useState([defaultTask])
   return (
     <div>
         <SearchTask/>
         <div className="mt-[25px] rounded-xl border-[rgba(206,206,206,0.12)] bg-[#1D212B] p-[30px]">
         <TaskAction/>
         <TaskList/>
+
+       
         </div>
  
     </div>
